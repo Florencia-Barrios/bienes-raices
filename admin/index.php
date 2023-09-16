dd9f632f2c19300debca3825e072c58cf4bff75b
Florencia Barrios <florenciabarrios.n@gmail.com>
2023-09-16 22:18:39 +0200

    button new property index.php

--- admin/index.php
+++ admin/index.php
@@ -5,6 +5,9 @@ incluirTemplate('header');
 
 <main class="contenedor seccion">
     <h1>Administrador de Bienes Raíces</h1>
+
+    <a href="/admin/propiedades/crear.php" class="boton boton-verde">Nueva Propiedad</a>
+
 </main>
 
 <?php
