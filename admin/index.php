207774062a98418f402e9e82de7905968ce24bb0
Florencia Barrios <florenciabarrios.n@gmail.com>
2023-09-16 20:27:17 +0200

    update link require index.php/admin

---
+++ admin/index.php
@@ -0,0 +1,12 @@
+<?php
+require '../includes/funciones.php';
+incluirTemplate('header');
+?>
+
+<main class="contenedor seccion">
+    <h1>Administrador de Bienes Raíces</h1>
+</main>
+
+<?php
+incluirTemplate('footer');
+?>
